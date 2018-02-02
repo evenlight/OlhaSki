ce2aa2068470bbc1354e6e8344cbdae7e89a9a84
des <web@evenlight.team>
2018-02-02 13:10:29 +0800

    hotfix

--- admin/require/functions.php
+++ admin/require/functions.php
@@ -13,4 +13,25 @@
         
         unset($url, $result);
     }
+    function upload_file ($start, $type)
+    {
+        if ($start==1)
+        {
+            $uploaddir = $_SERVER['DOCUMENT_ROOT'].'/Content/img/';
+            $filename = uniqid().'.jpg';
+            $uploadfile = $uploaddir . $filename;
+            move_uploaded_file($_FILES['img']['tmp_name'], $uploadfile);
+            
+            if ($type=="U")
+            {
+                $img=", img='".$filename."'";
+            }
+            elseif ($type=="I")
+            {
+                $img=$filename;
+            }
+
+            return $img;
+        }
+    }
 ?>
