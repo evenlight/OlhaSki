b486f0a6273211b648a11b659a9cb43f9e75664f
des <web@evenlight.team>
2018-02-06 21:10:42 +0800

    hotfix

--- admin/require/login.php
+++ admin/require/login.php
@@ -1,37 +1,24 @@
 <?php
     if ($_POST['command']=='authorize')
     {
-        //Пишем логин и пароль из формы в переменные (для удобства работы):
         $login = $_POST['login'];
         $password = $_POST['password'];
         $query = 'SELECT * FROM users WHERE login="'.$login.'" AND password="'.$password.'"';
-        //echo $query;
-        $result = mysqli_query($conn, $query); //ответ базы запишем в переменную $result
-        if ($result)
-        {
-           // echo 'true';
-        }
+        $result = mysqli_query($conn, $query);
         while ($row=mysqli_fetch_assoc($result))
         {
             $user_id=$row['id'];
         }
-        //$user = mysqli_fetch_assoc($result); //преобразуем ответ из БД в нормальный массив PHP
 
-        //Если база данных вернула не пустой ответ - значит пара логин-пароль правильная
         if (!empty($user_id)) 
         {
             session_start(); 
             $_SESSION['user'] = $user_id; 
             echo '<SCRIPT language="JavaScript"> window.location="/admin/home/";</SCRIPT>';
-            //require_once('menu.php');
-
         } 
         else 
         {
             echo '<div class="alert alert-danger signin-form">Неверно указан логин/пароль!</div>';
-            //$_SESSION['auth'] = false;
-            //die();
-            //Пользователь неверно ввел логин или пароль, выполним какой-то код.
         }
     }
 ?>
