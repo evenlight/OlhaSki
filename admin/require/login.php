48398869f4b95f6f44336adf6efa187ddfd9bb24
des <web@evenlight.team>
2018-02-06 21:01:30 +0800

    login

--- admin/require/login.php
+++ admin/require/login.php
@@ -1,34 +1,37 @@
 <?php
     if ($_POST['command']=='authorize')
     {
-
         //Пишем логин и пароль из формы в переменные (для удобства работы):
         $login = $_POST['login'];
         $password = $_POST['password'];
-
-        /*
-            Формируем и отсылаем SQL запрос:
-            ВЫБРАТЬ ИЗ таблицы_users ГДЕ поле_логин = $login И поле_пароль = $password.
-        */
         $query = 'SELECT * FROM users WHERE login="'.$login.'" AND password="'.$password.'"';
         //echo $query;
         $result = mysqli_query($conn, $query); //ответ базы запишем в переменную $result
-        $user = mysqli_fetch_assoc($result); //преобразуем ответ из БД в нормальный массив PHP
+        if ($result)
+        {
+           // echo 'true';
+        }
+        while ($row=mysqli_fetch_assoc($result))
+        {
+            $user_id=$row['id'];
+        }
+        //$user = mysqli_fetch_assoc($result); //преобразуем ответ из БД в нормальный массив PHP
 
         //Если база данных вернула не пустой ответ - значит пара логин-пароль правильная
-        if (!empty($user)) 
+        if (!empty($user_id)) 
         {
             session_start(); 
-            $_SESSION['auth'] = true; 
-            //echo '<SCRIPT language="JavaScript"> window.location="/admin/home/";</SCRIPT>';
-            require_once('menu.php');
+            $_SESSION['user'] = $user_id; 
+            echo $_SESSION['user'];
+            echo '<SCRIPT language="JavaScript"> window.location="/admin/home/";</SCRIPT>';
+            //require_once('menu.php');
 
         } 
         else 
         {
-            echo '<div class="alert alert-danger">sdkljfskjsdk</div>';
-            $_SESSION['auth'] = false;
-            die();
+            echo '<div class="alert alert-danger">Неверно указан логин/пароль!</div>';
+            //$_SESSION['auth'] = false;
+            //die();
             //Пользователь неверно ввел логин или пароль, выполним какой-то код.
         }
     }
--- admin/require/security.php
+++ admin/require/security.php
@@ -1,19 +1,36 @@
 <?php
-    //require_once('config.php');
-    if (isset($_SESSION['auth'])==false)
+    session_start(); 
+    if (isset($_SESSION['user'])) 
     {
-        require_once('form.php');  
-        require_once('login.php'); 
+        require_once('menu.php');
+        //echo $_SESSION['user'];
+    } 
+    else 
+    {
+        require_once('form.php');
+        require_once('login.php');
         die();
     }
-    else
-    {
-            
-        
-    }
+     
     if (@$_GET['action']=='logout')
     {
+        session_start();
+        unset($_SESSION['user']);
         session_destroy();
-        //echo '<SCRIPT language="JavaScript"> window.location="/admin/home/";</SCRIPT>';
+        echo '<SCRIPT language="JavaScript"> window.location="/admin/home/";</SCRIPT>';
     }
+
+    /*require_once('login.php');
+    if (!isset($_SESSION['auth']))
+    {
+        require_once('form.php');  
+        die();
+    }
+    elseif(isset($_SESSION['auth']))
+    {
+        require_once('menu.php');
+    }
+    
+    require_once('login.php');
+*/
 ?>
