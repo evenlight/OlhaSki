d1794eca25c6f7a9f7e1ea750c12456303d1cea6
des <web@evenlight.team>
2018-02-10 22:23:18 +0800

    fix gallery

--- admin/require/functions.php
+++ admin/require/functions.php
@@ -12,7 +12,7 @@
     {
         if ($result==1)
         {
-            if ($url!='')
+            if ($url!=0)
             {
                 echo '<div class="alert alert-success col-md-8 col-md-offset-2">Действие выполнено успешно</div>';
                 echo '<SCRIPT language="JavaScript"> window.location="/admin/'.$url.'/";</SCRIPT>';
