36c072c692e01e84aa1933fa2ee1b99a3668a789
des <web@evenlight.team>
2018-03-05 14:08:39 +0800

    resize images for gallery

--- admin/gallery/upload.php
+++ admin/gallery/upload.php
@@ -1,16 +1,28 @@
 <?php
 require_once('../require/security.php');
+require_once('../require/functions.php');
 $storeFolder = $uploaddir;
+// Пути загрузки файлов
+$tmp_path = '../tmp/';
+$path = '../../content/gallery/';
+//$path = '../img/';
 if (!empty($_FILES)) 
 {
-    require_once('../require/config.php');
-    $uploaddir = $_SERVER['DOCUMENT_ROOT'].'/content/gallery/';
+    /*$uploaddir = $_SERVER['DOCUMENT_ROOT'].'/content/gallery/';
     $filename = uniqid().'.jpg';
     $uploadfile = $uploaddir . $filename;
     move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile);
-    $img=$filename;
+    $img=$filename;*/
 
-    $sql="insert into gallery (albumid, img) values (".$_GET['pid'].", '".$img."')";
+    $name = resize($_FILES['file'], 2, '');
+    // Удаляем временный файл
+    $nameImg=uniqid().'.jpg';
+    copy($tmp_path . $name, $path . $nameImg);
+
+    unlink($tmp_path . $name);
+
+    require_once('../require/config.php');
+    $sql="insert into gallery (albumid, img) values (".$_GET['pid'].", '".$nameImg."')";
     $execute=mysqli_query($conn, $sql);
 }
 ?>
--- admin/require/functions.php
+++ admin/require/functions.php
@@ -167,4 +167,73 @@ if ($currentfile=='add.html' or $currentfile=='edit.html')
         
         $sxe->asXML($xmlFile);
     }
+
+    function resize($file, $type = 1, $rotate = null, $quality = null)
+    {
+        global $tmp_path;
+
+        // Ограничение по ширине в пикселях
+        $max_thumb_size = 200;
+        $max_size = 800;
+
+        // Качество изображения по умолчанию
+        if ($quality == null)
+            $quality = 75;
+
+        // Cоздаём исходное изображение на основе исходного файла
+        if ($file['type'] == 'image/jpeg')
+            $source = imagecreatefromjpeg($file['tmp_name']);
+        elseif ($file['type'] == 'image/png')
+            $source = imagecreatefrompng($file['tmp_name']);
+        elseif ($file['type'] == 'image/gif')
+            $source = imagecreatefromgif($file['tmp_name']);
+        else
+            return false;
+
+        // Поворачиваем изображение
+        if ($rotate != null)
+            $src = imagerotate($source, $rotate, 0);
+        else
+            $src = $source;
+
+        // Определяем ширину и высоту изображения
+        $w_src = imagesx($src);
+        $h_src = imagesy($src);
+
+        // В зависимости от типа (эскиз или большое изображение) устанавливаем ограничение по ширине.
+        if ($type == 1)
+            $w = $max_thumb_size;
+        elseif ($type == 2)
+            $w = $max_size;
+
+        // Если ширина больше заданной
+        if ($w_src > $w)
+        {
+            // Вычисление пропорций
+            $ratio = $w_src/$w;
+            $w_dest = round($w_src/$ratio);
+            $h_dest = round($h_src/$ratio);
+
+            // Создаём пустую картинку
+            $dest = imagecreatetruecolor($w_dest, $h_dest);
+
+            // Копируем старое изображение в новое с изменением параметров
+            imagecopyresampled($dest, $src, 0, 0, 0, 0, $w_dest, $h_dest, $w_src, $h_src);
+
+            // Вывод картинки и очистка памяти
+            imagejpeg($dest, $tmp_path . $file['name'], $quality);
+            imagedestroy($dest);
+            imagedestroy($src);
+
+            return $file['name'];
+        }
+        else
+        {
+            // Вывод картинки и очистка памяти
+            imagejpeg($src, $tmp_path . $file['name'], $quality);
+            imagedestroy($src);
+
+            return $file['name'];
+        }
+    }
 ?>
