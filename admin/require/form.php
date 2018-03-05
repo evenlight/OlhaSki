89b0d49a207ddcb86a967eafd77530bb6faf54e5
des <web@evenlight.team>
2018-03-05 15:25:27 +0800

    Add recovery password

--- admin/require/form.php
+++ admin/require/form.php
@@ -5,5 +5,6 @@
     <input type="text" id="inputLogin" name="login" class="form-control" placeholder="Логин" required autofocus>
     <label for="inputPassword" class="sr-only">Пароль</label>
     <input type="password" id="inputPassword" name ="password" class="form-control" placeholder="Пароль" required>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button><br>
+    <div align="center"><a href="../users/recovery.html">Забыли пароль?</a></div>
 </form>
